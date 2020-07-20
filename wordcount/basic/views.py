eec186d8121ee677e5a65b3d0a4fdb21fc8e53ba
yo14 <ifliandry11@gmail.com>
2020-07-20 16:31:07 +0700

    render content to templates

--- wordcount/basic/views.py
+++ wordcount/basic/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'home.html', {"welcome":"Halo selamat datang di Depok"})
 
 def eggs(request):
     return HttpResponse('<h1>I like to eat eggs</h1>')
