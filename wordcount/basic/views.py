180dc9aae3f9ba8381357c58b6242f12229368cb
yo14 <ifliandry11@gmail.com>
2020-07-21 05:55:54 +0700

    Making word count, and show it on browser

--- wordcount/basic/views.py
+++ wordcount/basic/views.py
@@ -1,3 +1,5 @@
+import operator
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,4 +7,19 @@ def home(request):
     return render(request, 'home.html')
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word] += 1
+        else:
+            #add to the dictionary
+            worddictionary[word] = 1
+
+    sorted_words = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {"fulltext":fulltext, "count": len(wordlist), "sorted_words": sorted_words})
+
